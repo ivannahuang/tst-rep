34cc7c5f4a1cb22f0f8f1f82f3b91804036fa51a
ivannahuang <huang.ivanna@gmail.com>
2020-02-19 15:48:22 +0800

    spike to smoothed and trucated version, change smooth width to kernel width

--- useful func/MakeDataSinglNeuronSpikeFeatures.m
+++ useful func/MakeDataSinglNeuronSpikeFeatures.m
@@ -1,9 +1,9 @@
 function  [y_spike, y_smspike] ...
-    =  MakeDataSinglNeuronSpikeFeatures(spike,t1,t2,smoothwidth)
-
+    =  MakeDataSinglNeuronSpikeFeatures(spike,t1,t2,kernelwidth)
+% smoothes the data in each column of x
 % spike sequence from t1 to t2, and gaussian smooth
 y_spike = spike(t1:t2,1);
 
 
 % smoothversion
-y_smspike = smoothdata(y_spike,'gaussian',smoothwidth);
+y_smspike = func_GaussianSmooth(y_spike,kernelwidth);
